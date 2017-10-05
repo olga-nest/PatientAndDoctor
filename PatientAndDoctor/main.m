f86ab281389d52b0614c161b769cf8a707802cfd
olga-nest <olga.w.nesterova@gmail.com>
2017-10-05 14:03:35 -0700

    Initial Commit

---
+++ PatientAndDoctor/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PatientAndDoctor
+//
+//  Created by Olga on 10/5/17.
+//  Copyright © 2017 Olga. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
